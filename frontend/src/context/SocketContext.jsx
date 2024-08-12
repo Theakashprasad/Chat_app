bac2ddeeb4e3856aec9907c0cec912881d44f4be
akashh <akashyoungstar123@gmail.com>
2024-08-13 00:14:25 +0530

    third

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-gcr0.onrender.com/", {
+			const socket = io("https://chat-app-jetv.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
